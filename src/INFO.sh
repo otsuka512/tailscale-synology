413a5aabce17e63e46655db11ce60db6680f2833
David Crawshaw <crawshaw@tailscale.com>
2021-03-11 11:53:44 -0500

    CGI script for Tailscale App on Synology.
    Lets a user login using the Synology GUI.
    
    Signed-off-by: David Crawshaw <crawshaw@tailscale.com>

--- src/INFO.sh
+++ src/INFO.sh
@@ -3,6 +3,7 @@
 VERSION=$1
 ARCH=$2
 PKG_SIZE=$3
+DSM_VERSION=$4
 
 TIMESTAMP=$(date -u +%Y%m%d-%H:%M:%S)
 
@@ -11,34 +12,45 @@ TIMESTAMP=$(date -u +%Y%m%d-%H:%M:%S)
 # https://github.com/SynologyOpenSource/pkgscripts-ng/tree/master/include platform.<PLATFORM> files
 case $ARCH in
 amd64)
-  PLATFORMS="x64 x86 apollolake avoton braswell broadwell broadwellnk bromolow cedarview denverton dockerx64 grantley purley kvmx64 x86_64"
+  PLATFORMS="x86_64"
   ;;
 386)
-  PLATFORMS="evansport"
+  PLATFORMS="i686"
   ;;
 arm64)
-  PLATFORMS="aarch64 armv8 rtd1296 armada37xx"
+  PLATFORMS="armv8"
   ;;
 arm)
-  PLATFORMS_ARM5="armv5 88f6281 88f628x"
-  PLATFORMS_ARM7="armv7 alpine armada370 armada375 armada38x armadaxp comcerto2k monaco hi3535 ipq806x northstarplus dakota"
-  PLATFORMS="${PLATFORMS_ARM5} ${PLATFORMS_ARM7}"
+  PLATFORMS="armv7"
   ;;
+  # TODO, each separate: armada370, armada375, armada38x, armadaxp, comcerto2k, monaco
 *)
-  # PLATFORMS_PPC="powerpc ppc824x ppc853x ppc854x qoriq"
   echo "Unsupported architecture: ${ARCH}"
   exit 1
   ;;
 esac
 
+if [ "$DSM_VERSION" = "6" ]; then
+  os_min_ver="6.0.1-7445"
+  os_max_ver="7.0-40000"
+else
+  os_min_ver="7.0-40000"
+  os_max_ver=""
+fi
+
 cat <<EOF
-package="tailscale"
+package="Tailscale"
 version="${VERSION}"
 arch="${PLATFORMS}"
 description="Connect all your devices using WireGuard, without the hassle."
 displayname="Tailscale"
-maintainer="nirev"
+maintainer="Tailscale"
 maintainer_url="https://github.com/tailscale/tailscale-synology"
 create_time="${TIMESTAMP}"
-extractsize=${PKG_SIZE}
+dsmuidir="ui"
+dsmappname="SYNO.SDS.Tailscale"
+startstop_restart_services="nginx"
+os_min_ver="${os_min_ver}"
+os_max_ver="${os_max_ver}"
+extractsize="${PKG_SIZE}"
 EOF
